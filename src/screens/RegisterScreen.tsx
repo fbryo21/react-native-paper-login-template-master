7c70a7a4057f81f8055befe921771cd35f0380f6
fbryo21 <febryryo21@gmail.com>
2022-06-04 10:04:57 +0700

    tes

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -51,7 +51,7 @@ const RegisterScreen = ({ navigation }: Props) => {
         returnKeyType="next"
         value={name.value}
         onChangeText={text => setName({ value: text, error: '' })}
-        error={!!name.error}
+        error={!!name.errors}
         errorText={name.error}
       />
 
